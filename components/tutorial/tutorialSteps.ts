c47981bbd876c3c340a1d8d3608cba0687032424
Alonzo <ramos.alonzoraniel@gmail.com>
2026-08-03 21:35:17 +0800

    feat: add 6-step editor tutorial screenshots

--- components/tutorial/tutorialSteps.ts
+++ components/tutorial/tutorialSteps.ts
@@ -1,7 +1,10 @@
 const tutorialSteps = [
-    // require('../../assets/tutorial/step1.png'),
-    require('../../assets/bg1.jpg'),
-
+    require('../../assets/tutorial/1.png'),
+    require('../../assets/tutorial/2.png'),
+    require('../../assets/tutorial/3.png'),
+    require('../../assets/tutorial/4.png'),
+    require('../../assets/tutorial/5.png'),
+    require('../../assets/tutorial/6.png'),
 ];
 
 export default tutorialSteps;
